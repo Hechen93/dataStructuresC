fe2c47308c54a2c46664f0f0e6ff7ce0877e99fa
Hechen93 <48193329+Hechen93@users.noreply.github.com>
2021-12-14 23:34:41 -0700

    more code

--- euclidGCD.cpp
+++ euclidGCD.cpp
@@ -39,15 +39,42 @@ int calcGcd(int a, int b) {
     return c;
 };
 
+//Bruce Schneier's multiple GCD implementation in C
+int multipleGCD( int m, int *x) {
+    size_t i;
+    int g;
+
+    if (m < 1) {
+        return 0;
+    }
+
+    g = x[0];
+
+    for(i = 1; i < m; ++i){
+        g = calcGcd(g, x[i]);
+/* optimization, since for random x[i], g == 1 60% of the time: */
+        if (g == 1) {
+            return 1;
+        }
+        return g;
+    }
+}
+
 
 int main(int argc, char *argv[]){
 
-    int a, b, c;
+    int a, b, c, e;
+
+    int d[5] = {13, 26, 39, 52, 65};
 
     cout<<"Enter two numbers: "<<endl;
     cin>>a;
     cin>>b;
     c = calcGcd(a,b);
-    cout<<"GCD is: " << c << endl;
+    cout<<"GCD1 is: " << c << endl;
+
+    e = multipleGCD(b, d);
+
+    cout<<"GCD2 is: " << e << endl;
 
 }
